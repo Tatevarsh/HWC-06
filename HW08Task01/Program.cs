2c90280ece3d3875fd8224ee7cb07ab71d4cdf79
Tatevik <tshavarshevna@gmail.com>
2023-02-24 21:16:32 +0300

    tasks54586062

---
+++ HW08Task01/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию 
+// элементы каждой строки двумерного массива.
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
+void SwapColomn(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+Console.Write("Введите кол-во строк: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Введите кол-во столбцов: ");
+int col = int.Parse(Console.ReadLine()!);
+
+int[,] array2D = GetArray(row, col, -10, 10);
+PrintArray(array2D);
+Console.WriteLine();
+SwapColomn(array2D);
+PrintArray(array2D);
---
+++ HW08Task03/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+
+Console.Clear();
+
+
+int m = 2, n = 2, p = 2, q = 2, i, j;
+int[,] a = { { 2, 4 }, { 3, 2 } };
+int[,] b = { { 3, 4 }, { 3, 3 } };
+
+Console.WriteLine("Matrix a:");
+for (i = 0; i < m; i++)
+{
+    for (j = 0; j < n; j++)
+    {
+        Console.Write(a[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Matrix b:");
+for (i = 0; i < p; i++)
+{
+    for (j = 0; j < q; j++)
+    {
+        Console.Write(b[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+if (n != p)
+{
+    Console.WriteLine("не возможно");
+}
+else
+{
+    int[,] c = new int[m, q];
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < q; j++)
+        {
+            c[i, j] = 0;
+            for (int k = 0; k < n; k++)
+            {
+                c[i, j] += a[i, k] * b[k, j];
+            }
+        }
+    }
+    Console.WriteLine("______");
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            Console.Write(c[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
